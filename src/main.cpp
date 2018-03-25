6972da00be0fc834d7e7287e42c47f41ffe0785e
Tazzari Gabriele <tazzarig@MOBTEK227-OSX.station>
2018-03-25 21:59:21 +0200

    added interrupt management, removed delays

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,8 @@
-
-
-/***************************************************
-  This is a library for our I2C LED Backpacks
-
-  Designed specifically to work with the Adafruit LED Matrix backpacks
-  ----> http://www.adafruit.com/products/872
-  ----> http://www.adafruit.com/products/871
-  ----> http://www.adafruit.com/products/870
-
-  These displays use I2C to communicate, 2 pins are required to
-  interface. There are multiple selectable I2C addresses. For backpacks
-  with 2 Address Select pins: 0x70, 0x71, 0x72 or 0x73. For backpacks
-  with 3 Address Select pins: 0x70 thru 0x77
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
-
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
 #include <SPI.h>
+
+volatile bool interr=false;
 
 class Snake {
  public:
@@ -49,11 +27,19 @@ class Snake {
 };
 
 const char moves[]="ulrd";
-int countr=0;
+long countr=0;
+long lastclick=0;
+long lastmove=0;
 Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
 Snake mysnake = Snake(8,8);
 
+void changeDirection() {
+  interr=true;
+}
+
 void setup() {
+  pinMode (2,INPUT_PULLUP);
+  attachInterrupt(0,changeDirection,FALLING); // interrupt 0 is connected to PIN 2
   Serial.begin(9600);
   Serial.println("8x8 LED Matrix Test");
   matrix.begin(0x70);  // pass in the address
@@ -64,11 +50,10 @@ void setup() {
   delay (1000);
   matrix.clear();
   matrix.writeDisplay();  // write the changes we just made to the display
-  randomSeed(analogRead(0));
-  pinMode (2,INPUT_PULLUP);
-  pinMode (3,INPUT_PULLUP);
-}
+  //randomSeed(analogRead(0));
 
+}
+//smiles
 static const uint8_t PROGMEM
   smile_bmp[] =
   { B00111100,
@@ -84,25 +69,12 @@ static const uint8_t PROGMEM
     B01000010,
     B10100101,
     B10000001,
-    B10111101,
-    B10000001,
+    B10011001,
+    B10100101,
     B01000010,
-    B00111100 },
-  unic_bmp[] =
-  { B00001000,
-    B00101010,
-    B00111110,
-    B00011100,
-    B00111110,
-    B00111110,
-    B00111110,
-    B00011100 };
+    B00111100 };
 
 uint8_t l[64];
-
-
-
-
 Snake::Snake (uint16_t w, uint16_t h)
 {
   _width=w;
@@ -189,9 +161,20 @@ Snake::~Snake(void) {
   if (buffer) free (buffer);
 }
 
+
 void loop() {
   countr++;
+
+  if (interr) {
+    Serial.println("interrupt triggered");
+    if((millis()-lastclick)>150) {
+        mysnake.setDirection('r');
+        lastclick=millis();
+    }
+    interr=false;
+  }
   /*
+  //RANDOM MODE
   int ran2=random(3)+1;
   if (countr%ran2==0) {
     int ran=random(4);
@@ -199,76 +182,37 @@ void loop() {
   }
 */
   //da fare circuito antirimbalzo a 4 pulsanti
-  if (digitalRead(2)==LOW) {
-    mysnake.setDirection('l');
-  }
-  if (digitalRead(3)==LOW) {
-    mysnake.setDirection('r');
-  }
+  //if (digitalRead(2)==LOW) {
+  //  mysnake.setDirection('r');
+  //}
+  //if (digitalRead(3)==LOW) {
+  //  mysnake.setDirection('r');
+  //}
 
   if (!mysnake.gameover) {
-    Serial.println("posx: "+(String)mysnake.posx+" posy:"+(String)mysnake.posy);
-    Serial.println("Appx: "+(String)mysnake.applex+" Appy:"+(String)mysnake.appley);
+    //Serial.println("posx: "+(String)mysnake.posx+" posy:"+(String)mysnake.posy);
+    //Serial.println("Appx: "+(String)mysnake.applex+" Appy:"+(String)mysnake.appley);
 
-    matrix.drawPixel(mysnake.applex, mysnake.appley, LED_ON);
-    matrix.drawPixel(mysnake.posx, mysnake.posy, LED_ON);
-    matrix.drawPixel(mysnake.todelx, mysnake.todely, LED_OFF);
-    mysnake.nextmove();
+    if ((millis()-lastmove)>150) {
+          matrix.drawPixel(mysnake.applex, mysnake.appley, LED_ON);
+          matrix.drawPixel(mysnake.posx, mysnake.posy, LED_ON);
+          matrix.drawPixel(mysnake.todelx, mysnake.todely, LED_OFF);
+          mysnake.nextmove();
+          lastmove=millis();
+    }
+
 
   }
   else
   {
+    matrix.clear();
+    matrix.drawBitmap(0, 0, neutral_bmp, 8, 8, LED_ON);
+    matrix.writeDisplay();
     delay (5000);
     Serial.println("game over cycle");
     countr=0;
-    matrix.clear();
-    randomSeed(analogRead(0));
-    Snake mysnake = Snake(8,8);
   }
-    //matrix.drawBitmap(0, 0, neutral_bmp, 8, 8, LED_ON);
+    //
   matrix.writeDisplay();
-  delay(150);
 
-/*
-  matrix.clear();      // clear display
-  matrix.drawPixel(0, 0, LED_ON);
-  matrix.writeDisplay();  // write the changes we just made to the display
-  delay(500);
-
-  matrix.clear();
-  matrix.drawLine(0,0, 7,7, LED_ON);
-  matrix.writeDisplay();  // write the changes we just made to the display
-  delay(500);
-
-  matrix.clear();
-  matrix.drawRect(0,0, 8,8, LED_ON);
-  matrix.fillRect(2,2, 4,4, LED_ON);
-  matrix.writeDisplay();  // write the changes we just made to the display
-  delay(500);
-
-  matrix.clear();
-  matrix.drawCircle(3,3, 3, LED_ON);
-  matrix.writeDisplay();  // write the changes we just made to the display
-  delay(500);
-
-  matrix.setTextSize(1);
-  matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
-  matrix.setTextColor(LED_ON);
-  for (int8_t x=0; x>=-36; x--) {
-    matrix.clear();
-    matrix.setCursor(x,0);
-    matrix.print("Hello");
-    matrix.writeDisplay();
-    delay(40);
-  }
-  //matrix.setRotation(3);
-  for (int8_t x=7; x>=-36; x--) {
-    matrix.clear();
-    matrix.setCursor(x,0);
-    matrix.print("World");
-    matrix.writeDisplay();
-    delay(40);
-  }
-  matrix.setRotation(0);
-  */
 }
