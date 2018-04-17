8471f0b59995e82773823dff8f60518d77c298cd
gtchandra <gab@iMac27.station>
2018-04-18 00:14:14 +0200

    added trigger on pin3 to shutdown 4 battery power saving

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,15 @@ long countr=0;
 long lastmove=0;
 Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
 Snake mysnake = Snake(8,8);
+float th_low = 200;
+float th_high = 800;
 
 void setup() {
-  //control button connections (avoid 0,1 since serial comms may autofire buttons)
-  pinMode (2,INPUT_PULLUP);
-  pinMode (3,INPUT_PULLUP);
-  pinMode (4,INPUT_PULLUP);
-  pinMode (5,INPUT_PULLUP);
+  //control using analog joystick on A0 / A1 pins (hi-low thresholds)
+  pinMode (A0,INPUT);
+  pinMode (A1,INPUT);
+  pinMode (PD3,OUTPUT); //pin used to shutdown power
+  digitalWrite(PD3,HIGH);
   Serial.begin(9600);
   Serial.println("Snake Started");
   matrix.begin(0x70);  // pass in the address
@@ -48,48 +50,56 @@ static const uint8_t PROGMEM
 
 void loop() {
   countr++;
-  if (digitalRead(2)==LOW) {
+  if (analogRead(A0)>th_high) {
     mysnake.setDirection('u');
-    Serial.println("U");
+    countr=millis();
   }
-  if (digitalRead(3)==LOW) {
-    mysnake.setDirection('l');
-    Serial.println("L");
-  }
-  if (digitalRead(4)==LOW) {
-    mysnake.setDirection('r');
-    Serial.println("R");
-  }
-  if (digitalRead(5)==LOW) {
+  if (analogRead(A0)<th_low) {
     mysnake.setDirection('d');
-    Serial.println("D");
+    countr=millis();
   }
+  if (analogRead(A1)>th_high) {
+    mysnake.setDirection('r');
+    countr=millis();
+  }
+  if (analogRead(A1)<th_low) {
+    mysnake.setDirection('l');
+    countr=millis();
+  }
+  if ((millis()-countr)<20000) {
+          if (!mysnake.gameover) {
+            if ((millis()-lastmove)>100) {
+                  matrix.drawPixel(mysnake.applex, mysnake.appley, LED_ON);
+                  matrix.drawPixel(mysnake.posx, mysnake.posy, LED_ON);
+                  matrix.drawPixel(mysnake.todelx, mysnake.todely, LED_OFF);
+                  mysnake.nextmove();
+                  lastmove=millis();
+            }
+          }
+          else
+          {
+            matrix.clear();
+            matrix.drawBitmap(0, 0, neutral_bmp, 8, 8, LED_ON);
+            matrix.writeDisplay();
+            delay (1000);
 
-  if (!mysnake.gameover) {
-    if ((millis()-lastmove)>100) {
-          matrix.drawPixel(mysnake.applex, mysnake.appley, LED_ON);
-          matrix.drawPixel(mysnake.posx, mysnake.posy, LED_ON);
-          matrix.drawPixel(mysnake.todelx, mysnake.todely, LED_OFF);
-          mysnake.nextmove();
-          lastmove=millis();
-    }
-  }
-  else
-  {
-    matrix.clear();
-    matrix.drawBitmap(0, 0, neutral_bmp, 8, 8, LED_ON);
-    matrix.writeDisplay();
-    delay (1000);
-
-    for (int8_t x=7; x>=-60; x--) {
-        matrix.clear();
-        matrix.setCursor(x,0);
-        matrix.print("Score:"+(String)mysnake.length);
-        matrix.writeDisplay();
-        delay(100);
-    }
-    delay(1000);
-    countr=0;
-  }
-  matrix.writeDisplay();
+            for (int8_t x=7; x>=-60; x--) {
+                matrix.clear();
+                matrix.setCursor(x,0);
+                matrix.print("Score:"+(String)mysnake.length);
+                matrix.writeDisplay();
+                delay(100);
+            }
+            delay(1000);
+          }
+          matrix.writeDisplay();
+      }
+    else
+      {
+          matrix.clear();
+          matrix.writeDisplay();
+          digitalWrite(PD3,LOW); //shutdown
+          delay(1000);
+          digitalWrite(PD3,HIGH);
+      }
 }
